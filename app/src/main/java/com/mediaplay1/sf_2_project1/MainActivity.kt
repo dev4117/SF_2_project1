ad3d3ded98eb2589efab59f8cc9ccb416e7dc2f1
skypusher <das.audi@yandex.ru>
2021-11-26 17:18:35 +0300

    first_commit

---
+++ app/src/main/java/com/mediaplay1/sf_2_project1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mediaplay1.sf_2_project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
