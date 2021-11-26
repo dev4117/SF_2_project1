cff8851e46638a9b148d46aec552794cd3d5b8f5
skypusher <das.audi@yandex.ru>
2021-11-26 17:28:50 +0300

    second_commit

--- app/src/main/java/com/mediaplay1/sf_2_project1/MainActivity.kt
+++ app/src/main/java/com/mediaplay1/sf_2_project1/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.mediaplay1.sf_2_project1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
